5384040d68a83db5f5c0049b9a5285e163f38762
anidabilalli <ab32615@seeu.edu.mk>
2026-05-31 00:49:26 +0200

    Implemented Create

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,21 +1,95 @@
-import {movies} from "../utils/movies.js"
+import {useState} from "react";
+import { movies as moviesData } from "../utils/movies.js";
 
 function MoviesPage() {
+    const [movies, setMovies] = useState(moviesData);
+    const [title, setTitle] = useState("")
+    const [director, setDirector] = useState("")
+    const [genre, setGenre] = useState("")
+    const[watched, setWatched] = useState(false)
+
+    function addNewMovie() {
+        if (title !== "" && director !== "") {
+            const newMovie = {
+                id: movies.length + 1,
+                title: title,
+                director: director,
+                genre: genre,
+                watched: watched,
+            };
+
+            setMovies((prevState) => {
+                return [...prevState, newMovie];
+            });
+
+            setTitle("");
+            setDirector("");
+            setGenre("");
+            setWatched(false);
+        }
+    }
+
   return (
     <div className="p-8">
       <h1 className="text-3xl font-bold text-gray-900 mb-6">Movies</h1>
-        {
-            movies.map(movie => (
-                <div key={movie.id}>
-                    <h2>{movie.title}</h2>
-                        <p>Director:{movie.director}</p>
-                        <p>Genre:{movie.genre}</p>
-                        <p>Status:{movie.watched ? 'Watched': 'Not Watched'}</p>
-                </div>
-            ))
-        }
+
+       <div className="flex flex-col gap-4 items-start border border-pink-500 rounded p-2 m-4">
+           <input
+           className="border border-gray-700"
+           placeholder="Title..."
+           type="text"
+           name="title"
+           value={title}
+           onChange={(e) => setTitle(e.target.value)}
+           />
+           <input
+               className="border border-gray-700"
+               placeholder="Director..."
+               type="text"
+               name="director"
+               value={director}
+               onChange={(e) => setDirector(e.target.value)}
+           />
+           <input
+               className="border border-gray-700"
+               placeholder="Genre..."
+               type="text"
+               name="genre"
+               value={genre}
+               onChange={(e) => setGenre(e.target.value)}
+           />
+           <label className="flex items-center gap-2">
+               <input
+                   type="checkbox"
+                   name="watched"
+                   checked={watched}
+                   onChange={(e) => setWatched(e.target.checked)}
+                   />
+               Watched
+               </label>
+           <button
+               onClick={addNewMovie}
+               className="rounded bg-white font-bold text-pink-500 p-2 border-pink-700"
+               >
+               Add Movie
+           </button>
+       </div>
+        <div className="m-2">
+            {movies.map(movie => {
+                    return (
+                <div
+                key={movie.id}
+                className="border-pink-500 bg-pink-200 p-2 m-2 rounded">
+                <div className="text-lg font-bold text-red-700">{movie.title}</div>
+                <div>Director:{movie.director}</div>
+                <div>Genre:{movie.genre}</div>
+                <div>Status:{movie.watched ? 'Watched': 'Not Watched'}</div>
+           </div>
+            );
+        })}
     </div>
-  )
+</div>
+  );
 }
 
 export default MoviesPage
