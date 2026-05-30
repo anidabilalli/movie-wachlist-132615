66396b957382175705fae26fb217ab22c3af8936
anidabilalli <ab32615@seeu.edu.mk>
2026-05-30 23:35:09 +0200

    Created AuthContext

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,22 @@
+import {createContext, useState} from "react";
+
+export const AuthContext = createContext(null)
+export function AuthProvider({ children }) {
+    const [user, setUser] = useState(null)
+
+    const login = () => {
+        setUser({
+            name:"Anida Bilalli",
+            email:"ab32615@seeu.edu.mk"
+        });
+    };
+    const logout = () => {
+        setUser(null);
+    };
+
+    return <div>
+        <AuthContext.Provider value={{ user, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    </div>
+}
